28b4726cf28a93a98d8fb4c3b80d9413bd2d6747
mrpaulishaili <paulishaili@gmail.com>
2023-01-28 06:37:16 +0100

    ...

--- api/JAMB/AccreditedCenters.js
+++ api/JAMB/AccreditedCenters.js
@@ -5,14 +5,43 @@ const AccreditedCentersRouter = Router();
 
 // Get All
 AccreditedCentersRouter.get('/', async (req, res) => {
-  const limit = parseInt(req.query.limit) || 50;
-  const page = req.query.page || 0;
-  let startIndex = page * limit;
-  const centres = await ApprovedRegCentreModel.find()
-    .limit(limit)
-    .skip(startIndex);
+  try {
+    const limit = parseInt(req.query.limit) || 50;
+    const page = req.query.page - 1 || 0;
+    let startIndex = page * limit;
+    const endIndex = (page + 1) * limit;
+    const result = {};
+    const centres = await ApprovedRegCentreModel.find();
 
-  res.send(centres);
+    result.centres = centres;
+
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    if (endIndex < (await ApprovedRegCentreModel.find())) {
+      result.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    result.data = await ApprovedRegCentreModel.find()
+      .limit(limit)
+      .skip(startIndex)
+      .exec();
+
+    result.rowsPerPage = limit;
+
+    const data = res.json(result.data);
+
+    return data;
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Get One
@@ -34,3 +63,5 @@ AccreditedCentersRouter.patch('/', (req, res) => {
 // Delete One
 AccreditedCentersRouter.delete('/:id', async (req, res) => {});
 export default AccreditedCentersRouter;
+
+const SearchQuery = (':params', async (req, res) => {});
